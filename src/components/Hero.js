c77b6cc4a66689bff2b9bd7f69e8533df3e3ecdd
haaslaura <110221401+haaslaura@users.noreply.github.com>
2024-10-01 11:32:21 +0200

    All layouts css

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,28 @@
 import homeIllustration from '../assets/home-illustration.jpg'
+import { useState, useEffect } from 'react';
 
 function Hero() {
+
+    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 992);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth < 992);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <div className="herosection">
-            <h1>Chez vous, partout et ailleurs</h1>
-            <img className="hero" src={homeIllustration} alt="" />
+            <h1 className="herosection__title">
+                {isSmallScreen ? 'Chez vous,\n partout et ailleurs' : 'Chez vous, partout et ailleurs'}
+            </h1>
+            <img className="herosection__image" src={homeIllustration} alt="" />
         </div>
     );
 }
